b435ce718c014ee67f8ac435b74e366c327be2e7
ryukann <yoannlamouille@gmail.com>
2026-03-17 06:36:03 +0100

    Vériciation connexion et création token en dur

---
+++ js/auth/signin.js
@@ -0,0 +1,19 @@
+const mailInput = document.getElementById("EmailInput");
+const passwordInput = document.getElementById("PasswordInput");
+const btnSignin = document.getElementById("btnSignin");
+
+btnSignin.addEventListener("click",checkCredentials);
+
+function checkCredentials() {
+  //Ici, il faudra appeler une API pour vérifier les identifiants de l'utilisateur en BDD
+  if(mailInput.value == "test@mail.com" && passwordInput.value == "123") {
+    alert("Connexion réussie !");
+    //Ici, il faudra récupérer le token de l'utilisateur généré par l'API
+    window.location.replace("/");
+    const token = "lkjsdngfljsqdnglkjsdbglkjqskjgkfjgbqslkfdgbskldfgdfgsdgf"; 
+    //placer ce token en cookie
+  }else{
+    mailInput.classList.add("is-invalid");
+    passwordInput.classList.add("is-invalid");
+  }
+}
