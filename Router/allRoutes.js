b89092a0d5e8bb8fd4194672368691bf6d72f6c1
ryukann <yoannlamouille@gmail.com>
2026-03-23 15:13:32 +0100

    Eviter faille XSS injection html galerie

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,9 +3,9 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html",[]),
-    new Route("/galerie", "Galerie", "/pages/galerie.html",[]),
-    new Route("/signin", "Connexion", "/pages/auth/signin.html","[disconnected]","/js/auth/signin.js"),
-    new Route("/signup", "Inscription", "/pages/auth/signup.html","[disconnected]","/js/auth/signup.js"),
+    new Route("/galerie", "Galerie", "/pages/galerie.html",[],"/js/galerie.js"),
+    new Route("/signin", "Connexion", "/pages/auth/signin.html",["disconnected"],"/js/auth/signin.js"),
+    new Route("/signup", "Inscription", "/pages/auth/signup.html",["disconnected"],"/js/auth/signup.js"),
     new Route("/account", "Mon compte", "/pages/auth/account.html",["client","admin"]),
     new Route("/editPassword", "Changement de mot de passe", "/pages/auth/editPassword.html",["client","admin"]),
     new Route("/allResa", "Vos réservations", "/pages/reservations/allResa.html",["client"]),
--- js/script.js
+++ js/script.js
@@ -95,3 +95,8 @@ function showAndHideElementsForRoles(){
     })
 }
 
+function sanitizeHtml(text) {
+  const tempHtml = document.createElement('div');
+  tempHtml.textContent = text;
+  return tempHtml.innerHTML;
+}
